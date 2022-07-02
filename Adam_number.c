d9ab4e0fdd24184ad934bdd97144bad949e4f8ca
Srinivasmandalapu <97425298+Srinivasmandalapu@users.noreply.github.com>
2022-07-02 19:02:25 +0530

    #include<stdio.h> int main() {     int n,d,temp,rev=0,rev2=0,p,h;     scanf("%d",&n);     temp=n;     h=n*n;     while(n!=0)     {         d=n%10;         n=n/10;         rev=rev*10+d;     }     p=rev*rev;     while(p!=0)     {         d=p%10;         p=p/10;         rev2=rev2*10+d;     }     if(rev2==h)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Adam_number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,d,temp,rev=0,rev2=0,p,h;
+    scanf("%d",&n);
+    temp=n;
+    h=n*n;
+    while(n!=0)
+    {
+        d=n%10;
+        n=n/10;
+        rev=rev*10+d;
+    }
+    p=rev*rev;
+    while(p!=0)
+    {
+        d=p%10;
+        p=p/10;
+        rev2=rev2*10+d;
+    }
+    if(rev2==h)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
